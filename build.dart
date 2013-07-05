79cf38cdea072ae45aa706c9934641bdbea3cff6
= <=>
2013-07-05 10:15:56 +0000

    #8

--- build.dart
+++ build.dart
@@ -3,5 +3,5 @@ import 'package:web_ui/component_build.dart';
 
 // Ref: http://www.dartlang.org/articles/dart-web-components/tools.html
 main() {
-  build(new Options().arguments, ['web/bex.html','web/help.html']);
+  build(new Options().arguments, ['web/bex.html']);
 }
